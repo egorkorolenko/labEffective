2fe48b52af50f5bff2379399a1fa127a58ce5727
Egor Korolenko <korolenko.egor39@gmail.com>
2022-10-27 16:30:03 +0600

    Сделал экран героев

--- lab1effective/app/src/main/java/ru/korolenkoe/lab1effective/screens/HeroScreen.kt
+++ lab1effective/app/src/main/java/ru/korolenkoe/lab1effective/screens/HeroScreen.kt
@@ -1,13 +1,20 @@
 package ru.korolenkoe.lab1effective.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.ColorMatrix
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
@@ -16,19 +23,32 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import ru.korolenkoe.lab1effective.R
 import ru.korolenkoe.lab1effective.models.HeroItem
-import ru.korolenkoe.lab1effective.navigation.Screen
 
 @Composable
 fun HeroScreen(navController: NavController?, heroItem: HeroItem) {
+    val colorMatrix = ColorMatrix()
+    colorMatrix.setToSaturation(0f)
+
     Box(
         modifier = Modifier
             .fillMaxSize()
+            .paint(
+                painterResource(id = heroItem.image),
+                contentScale = ContentScale.Crop,
+                colorFilter = ColorFilter.colorMatrix(colorMatrix),
+                alpha = 0.3f
+            )
     ) {
         BackButton(navController)
 
-        Box {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
             HeroLogo()
         }
 
@@ -56,14 +76,27 @@ fun BackButton(navController: NavController?) {
     ) {
         Image(
             painter = painterResource(id = R.drawable.ic_baseline_arrow_back_24),
-            contentDescription = "back_button"
+            contentDescription = "back_button",
         )
     }
 }
 
 @Composable
 fun HeroLogo() {
-
+    Box(
+        modifier = Modifier
+            .fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data("https://www.tailorbrands.com/wp-content/uploads/2020/03/Captain-America.jpg")
+                .build(),
+            placeholder = painterResource(id = R.drawable.placeholder),
+            contentScale = ContentScale.Crop,
+            contentDescription = null,
+            modifier = Modifier.border(BorderStroke(4.dp, Color.Red))
+        )
+    }
 }
 
 @Composable
